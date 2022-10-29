353bd94135bfb244411bb6fa88af598b9641acb1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-29 12:52:46 +0300

    add test when thenReturn

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class CarTest {
 
@@ -10,6 +11,14 @@ class CarTest {
         Car newCar = Mockito.mock(Car.class);
 
         assertEquals(0, newCar.getYear());
+    }
+
+    @Test
+    void remoteServiceReturnValue() {
+        Car newCar = Mockito.mock(Car.class);
+        when(newCar.testInt(4)).thenReturn(10);
+
+        assertEquals(10, newCar.testInt(4));
     }
 
 }
